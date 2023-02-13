dfb08551c27840060b5f577897b2f96757f6a789
Jay <jay_on_business@outlook.com>
2023-02-13 20:39:00 +0800

    20230213

--- day01-mysql-basic/src/cn/mycloudway/sql/ddldemo3.sql
+++ day01-mysql-basic/src/cn/mycloudway/sql/ddldemo3.sql
@@ -1,1 +1,17 @@
--- Active: 1676195288091@@127.0.0.1@3306@db1
+SHOW TABLES;
+
+DROP TABLE tb_user;
+
+DROP TABLE IF EXISTS tb_user;
+
+ALTER TABLE student RENAME TO stu;
+
+ALTER TABLE stu ADD address VARCHAR(255);
+
+DESC stu;
+
+ALTER TABLE stu MODIFY address CHAR(50);
+
+ALTER TABLE stu CHANGE address addr VARCHAR(30);
+
+ALTER TABLE stu DROP addr;
