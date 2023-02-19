41280b707a07b636979aca81730ee3617ede5205
Jay <jay_on_business@outlook.com>
2023-02-19 23:21:15 +0800

    20230219

---
+++ day04-maven/src/main/java/cn/mycloudway/HelloWorld.java
@@ -0,0 +1,7 @@
+package cn.mycloudway;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Maven~~");
+    }
+}
---
+++ day04-maven/src/test/java/cn/mycloudway/TestMaven.java
@@ -0,0 +1,10 @@
+package cn.mycloudway;
+
+import org.junit.Test;
+
+public class TestMaven {
+    @Test
+    public void test() {
+        System.out.println("maven test =====>");
+    }
+}
