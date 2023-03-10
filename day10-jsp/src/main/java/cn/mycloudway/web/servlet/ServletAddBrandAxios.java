b234ae2b204b1651ff77428d42e234cfdfe93487
Jay <jay_on_business@outlook.com>
2023-03-11 00:01:06 +0800

    20230311

--- day10-jsp/src/main/java/cn/mycloudway/web/servlet/ServletAddBrandAxios.java
+++ day10-jsp/src/main/java/cn/mycloudway/web/servlet/ServletAddBrandAxios.java
@@ -2,25 +2,24 @@ package cn.mycloudway.web.servlet;
 
 import cn.mycloudway.pojo.Brand;
 import cn.mycloudway.service.BrandService;
+import com.alibaba.fastjson.JSON;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
 
 @WebServlet(urlPatterns = "/addBrandAxios")
 public class ServletAddBrandAxios extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String brandName = req.getParameter("brandName");
-        String companyName = req.getParameter("companyName");
-        int ordered = Integer.parseInt(req.getParameter("ordered"));
-        String description = req.getParameter("description");
-        int status = Integer.parseInt(req.getParameter("status"));
+        BufferedReader reader = req.getReader();
+        String payload = reader.readLine();
 
-        Brand brand = new Brand(brandName, companyName, ordered, description, status);
+        Brand brand = JSON.parseObject(payload, Brand.class);
         BrandService brandService = new BrandService();
         int result = brandService.add(brand);
 
